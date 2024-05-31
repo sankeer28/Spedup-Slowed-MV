967ce050caa8ec482f25cc8708c7b70f54a7705b
Sankeer <sankeer28@gmail.com>
2024-05-31 00:08:21 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='Spedup-Slowed-MV',
+version='0.1.0',
+author='sankeer',
+author_email='sankeer28@gmail.com',
+description='A Python script that automates the creation of nightcore-style/sped-up videos or slowed-down videos by combining a wallpaper with audio extracted from URLs from supported websites like YouTube, YouTube music, and Soundcloud.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.10',
+)
